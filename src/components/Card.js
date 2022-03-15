d83f2b79983a7bbb7a4c9fb9cf69a2aaed25b129
Tomo-ja <tomonao.hj@gmail.com>
2022-03-15 10:12:03 -0700

    style card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,19 @@
-import image from 
+import image from '../image/img_make-up.jpg'
+import starIcon from '../image/icon_star.svg'
 function Card (){
 	return(
-		<div>
+		<div className="card">
 			<img src={image} />
-
+			<div className="status">
+				<img src={starIcon} />
+				<span>5.0</span>
+				<span className="gray">(6) ・ </span>
+				<span className="gray">USA</span>
+			</div>
+			<p>Life lessons with Katie Zaferes</p>
+			<p><span className="bold">From $136</span> / person</p>
 		</div>
+
 	)
 }
 export default Card
