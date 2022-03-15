9d56fc3bec2e37b621670fa368e38059e348c735
Tomo-ja <tomonao.hj@gmail.com>
2022-03-14 19:02:15 -0700

    reset

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import image1 from './image/img_make-up.jpg'
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Hero />
-      <Card img={image1}
-            rating="5.0"
-            reviewCount={6}
-            country="USA"
-            title="Life Lessons with Katie Zaferes"
-            price={136}
-      />
+
+    <div className="App">
     </div>
   );
 }
