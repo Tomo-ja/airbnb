868a9edfcbd950206578c2bab4deb2651a10e56f
Tomo-ja <tomonao.hj@gmail.com>
2022-03-15 22:03:24 -0700

    up images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Header from './components/Header';
+import Card from './components/Card';
 import './App.css';
 
 function App() {
   return (
-
     <div className="App">
+        <Header />
+        <Card />
     </div>
   );
 }
